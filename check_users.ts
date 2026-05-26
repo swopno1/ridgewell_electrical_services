f9fe302ffb2b39e079ca23f60dd0a3d7ce88eb30
swopno1 <amirhossain.limon@gmail.com>
2026-05-26 12:22:23 +0600

    chore: remove obsolete documentation files and temporary scripts

--- check_users.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users.map(u => ({ email: u.email, role: u.role })));
-}
-main();
